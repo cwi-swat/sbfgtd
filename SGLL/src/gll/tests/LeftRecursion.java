8f2b91711ebc864a8fde88200739fb84afe45c43
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-03-23 10:04:19 +0000

    -Fixed sharing for tests.

--- SGLL/src/gll/tests/LeftRecursion.java
+++ SGLL/src/gll/tests/LeftRecursion.java
@@ -13,6 +13,7 @@ public class LeftRecursion extends SGLL{
 	private final static ParseStackNode NONTERMINAL_A0 = new NonTerminalParseStackNode("A", 0);
 	private final static ParseStackNode NONTERMINAL_A1 = new NonTerminalParseStackNode("A", 1);
 	private final static ParseStackNode TERMINAL_a2 = new TerminalParseStackNode("a".getBytes(), 2);
+	private final static ParseStackNode TERMINAL_a3 = new TerminalParseStackNode("a".getBytes(), 3);
 	
 	public LeftRecursion(byte[] input){
 		super(input);
@@ -25,7 +26,7 @@ public class LeftRecursion extends SGLL{
 	public void A(){
 		expect(NONTERMINAL_A1, TERMINAL_a2);
 		
-		expect(TERMINAL_a2);
+		expect(TERMINAL_a3);
 	}
 	
 	public static void main(String[] args){
