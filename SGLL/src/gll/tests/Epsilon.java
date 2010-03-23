7b68453318db4a93b0b03ce090101b997c8ca034
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-03-23 15:22:33 +0000

    -Made test consistent. Btw, epsilons + left recursion doesn't seem to work at the moment.

--- SGLL/src/gll/tests/Epsilon.java
+++ SGLL/src/gll/tests/Epsilon.java
@@ -9,20 +9,20 @@ S ::= Ae
 A ::= a
 */
 public class Epsilon extends SGLL{
-	private final static NonTerminalParseStackNode A0 = new NonTerminalParseStackNode("A", 0);
-	private final static TerminalParseStackNode epsilon1 = new TerminalParseStackNode("".getBytes(), 1);
-	private final static TerminalParseStackNode a2 = new TerminalParseStackNode("a".getBytes(), 2);
+	private final static NonTerminalParseStackNode NONTERMINAL_A0 = new NonTerminalParseStackNode("A", 0);
+	private final static TerminalParseStackNode EPSILON_1 = new TerminalParseStackNode("".getBytes(), 1);
+	private final static TerminalParseStackNode NONTERMINAL_a2 = new TerminalParseStackNode("a".getBytes(), 2);
 	
 	public Epsilon(byte[] input){
 		super(input);
 	}
 	
 	public void S(){
-		expect(A0, epsilon1);
+		expect(NONTERMINAL_A0, EPSILON_1);
 	}
 	
 	public void A(){
-		expect(a2);
+		expect(NONTERMINAL_a2);
 	}
 	
 	public static void main(String[] args){
