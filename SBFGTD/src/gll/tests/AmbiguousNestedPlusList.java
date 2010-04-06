0c654110a83f3b4da29097fb075059a4ec5ed97c
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-04-06 11:09:00 +0000

    -Added extra test.

---
+++ SBFGTD/src/gll/tests/AmbiguousNestedPlusList.java
@@ -0,0 +1,30 @@
+package gll.tests;
+
+import gll.SGLL;
+import gll.stack.CharListParseStackNode;
+import gll.stack.NonTerminalListParseStackNode;
+import gll.stack.ParseStackNode;
+
+public class AmbiguousNestedPlusList extends SGLL{
+	private final static ParseStackNode NONTERMINAL_LIST0 = new NonTerminalListParseStackNode(0, "A", true);
+	private final static ParseStackNode TERMINAL_LIST1 = new CharListParseStackNode(1, new char[][]{}, new char[]{'a'}, "[a]", true);
+	
+	public AmbiguousNestedPlusList(char[] input){
+		super(input);
+	}
+	
+	public void S(){
+		expect(NONTERMINAL_LIST0);
+	}
+	
+	public void A(){
+		expect(TERMINAL_LIST1);
+	}
+	
+	public static void main(String[] args){
+		AmbiguousNestedPlusList anpl = new AmbiguousNestedPlusList("aa".toCharArray());
+		System.out.println(anpl.parse("S"));
+		
+		System.out.println("S([A+(A([a]+([a](a))),A([a]+([a](a)))),A+(A([a]+([a](a),[a](a))))]) <- good");
+	}
+}
--- SBFGTD/src/gll/tests/NonTerminalStarList.java
+++ SBFGTD/src/gll/tests/NonTerminalStarList.java
@@ -11,7 +11,7 @@ A ::= a
 */
 public class NonTerminalStarList extends SGLL{
 	private final static ParseStackNode NONTERMINAL_LIST0 = new NonTerminalListParseStackNode(0, "A", false);
-	private final static ParseStackNode TERMINAL_A1 = new TerminalParseStackNode(new char[]{'a'}, 1);
+	private final static ParseStackNode TERMINAL_a1 = new TerminalParseStackNode(new char[]{'a'}, 1);
 	
 	public NonTerminalStarList(char[] input){
 		super(input);
@@ -22,7 +22,7 @@ public class NonTerminalStarList extends SGLL{
 	}
 	
 	public void A(){
-		expect(TERMINAL_A1);
+		expect(TERMINAL_a1);
 	}
 	
 	public static void main(String[] args){
