a43715b399a13a511429c62c82f63690fe02dbde
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-04-09 08:06:51 +0000

    -Changed optional tests to the new format.

--- SGLL/src/gll/tests/Optional2.java
+++ SGLL/src/gll/tests/Optional2.java
@@ -2,28 +2,35 @@ package gll.tests;
 
 import gll.SGLL;
 import gll.stack.LiteralParseStackNode;
+import gll.stack.NonTerminalParseStackNode;
 import gll.stack.OptionalParseStackNode;
 import gll.stack.ParseStackNode;
 
 /*
-S ::= a(a?)
+S ::= aO?
+O ::= a
 */
 public class Optional2 extends SGLL{
 	private final static ParseStackNode LITERAL_a0 = new LiteralParseStackNode(new char[]{'a'}, 0);
 	private final static ParseStackNode LITERAL_a1 = new LiteralParseStackNode(new char[]{'a'}, 1);
-	private final static ParseStackNode OPTIONAL_2 = new OptionalParseStackNode(2, LITERAL_a1, "a?");
+	private final static ParseStackNode NON_TERMINAL_O2 = new NonTerminalParseStackNode("O", 2);
+	private final static ParseStackNode OPTIONAL_3 = new OptionalParseStackNode(2, NON_TERMINAL_O2, "O?");
 	
 	public Optional2(char[] input){
 		super(input);
 	}
 	
 	public void S(){
-		expect(LITERAL_a0, OPTIONAL_2);
+		expect(LITERAL_a0, OPTIONAL_3);
+	}
+	
+	public void O(){
+		expect(LITERAL_a1);
 	}
 	
 	public static void main(String[] args){
 		Optional2 o2 = new Optional2("a".toCharArray());
 		System.out.println(o2.parse("S"));
-		System.out.println("S(a,a?()) <- good");
+		System.out.println("S(a,O?()) <- good");
 	}
 }
--- SGLL/src/gll/tests/Optional3.java
+++ SGLL/src/gll/tests/Optional3.java
@@ -16,26 +16,31 @@ public class Optional3 extends SGLL{
 	private final static ParseStackNode LITERAL_a2 = new LiteralParseStackNode(new char[]{'a'}, 2);
 	private final static ParseStackNode NONTERMINAL_A3 = new NonTerminalParseStackNode("A", 3);
 	private final static ParseStackNode NONTERMINAL_A4 = new NonTerminalParseStackNode("A", 4);
-	private final static ParseStackNode OPTIONAL_5 = new OptionalParseStackNode(5, NONTERMINAL_A3, "A?");
+	private final static ParseStackNode NON_TERMINAL_O5 = new NonTerminalParseStackNode("O", 5);
+	private final static ParseStackNode OPTIONAL_6 = new OptionalParseStackNode(6, NON_TERMINAL_O5, "O?");
 	
 	public Optional3(char[] input){
 		super(input);
 	}
 	
 	public void S(){
-		expect(LITERAL_a0, OPTIONAL_5);
+		expect(LITERAL_a0, OPTIONAL_6);
 		
-		expect(LITERAL_a1, NONTERMINAL_A4);
+		expect(LITERAL_a1, NONTERMINAL_A3);
 	}
 	
 	public void A(){
 		expect(LITERAL_a2);
 	}
 	
+	public void O(){
+		expect(NONTERMINAL_A4);
+	}
+	
 	public static void main(String[] args){
 		Optional3 o3 = new Optional3("aa".toCharArray());
 		System.out.println(o3.parse("S"));
-		System.out.println("[S(a,A?(A(a))),S(a,A(a))] <- good");
+		System.out.println("[S(a,O?(O(A(a)))),S(a,A(a))] <- good");
 	}
 }
 
