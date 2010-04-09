d5bb84667c5f3082ab2f8754cf4d8fb64c36897c
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-04-09 11:25:01 +0000

    -Cleanup.

--- SBFGTD/src/gll/tests/CharPlusList.java
+++ SBFGTD/src/gll/tests/CharPlusList.java
@@ -1,16 +1,14 @@
 package gll.tests;
 
 import gll.SGLL;
-import gll.stack.ParseStackNode;
 import gll.stack.CharListParseStackNode;
-import gll.stack.LiteralParseStackNode;
+import gll.stack.ParseStackNode;
 
 /*
 S ::= [a-z]+
 */
 public class CharPlusList extends SGLL{
 	private final static ParseStackNode CHAR_LIST0 = new CharListParseStackNode(0, new char[][]{{'a', 'z'}}, new char[]{}, "[a-z]", "[a-z]+", true);
-	private final static ParseStackNode LITERAL_a1 = new LiteralParseStackNode(new char[]{'a'}, 1);
 	
 	public CharPlusList(char[] input){
 		super(input);
@@ -18,10 +16,6 @@ public class CharPlusList extends SGLL{
 	
 	public void S(){
 		expect(CHAR_LIST0);
-	}
-	
-	public void List0(){
-		expect(LITERAL_a1);
 	}
 	
 	public static void main(String[] args){
