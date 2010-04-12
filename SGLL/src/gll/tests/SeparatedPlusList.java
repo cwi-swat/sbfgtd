93b761d8bd265e3fa1ed6c706f8af0b2fc0975dc
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-04-12 12:10:07 +0000

    -Fixed the separated plus list test.

--- SGLL/src/gll/tests/SeparatedPlusList.java
+++ SGLL/src/gll/tests/SeparatedPlusList.java
@@ -1,7 +1,6 @@
 package gll.tests;
 
 import gll.SGLL;
-import gll.stack.CharStackNode;
 import gll.stack.LiteralStackNode;
 import gll.stack.NonTerminalStackNode;
 import gll.stack.SeparatedListStackNode;
@@ -9,8 +8,8 @@ import gll.stack.StackNode;
 
 public class SeparatedPlusList extends SGLL{
 	private final static StackNode NONTERMINAL_A0 = new NonTerminalStackNode("A", 0);
-	private final static StackNode CHAR_b1 = new CharStackNode(new char[][]{}, new char[]{'b'}, 1, "b");
-	private final static StackNode LIST2 = new SeparatedListStackNode(2, NONTERMINAL_A0, new StackNode[]{CHAR_b1}, "(Ab)+", true);
+	private final static StackNode LITERAL_b1 = new LiteralStackNode(new char[]{'b'}, 1);
+	private final static StackNode LIST2 = new SeparatedListStackNode(2, NONTERMINAL_A0, new StackNode[]{LITERAL_b1}, "(Ab)+", true);
 	private final static StackNode LITERAL_a3 = new LiteralStackNode(new char[]{'a'}, 3);
 	
 	public SeparatedPlusList(char[] input){
@@ -26,9 +25,9 @@ public class SeparatedPlusList extends SGLL{
 	}
 	
 	public static void main(String[] args){
-		SeparatedPlusList nrpl = new SeparatedPlusList("aa".toCharArray());
+		SeparatedPlusList nrpl = new SeparatedPlusList("ababa".toCharArray());
 		System.out.println(nrpl.parse("S"));
 		
-		System.out.println("S((Ab)+((Ab)+((Ab)+(A(a)),A(ba)),A(ba))) <- good");
+		System.out.println("S((Ab)+((Ab)+((Ab)+(A(a)),b,A(a)),b,A(a))) <- good");
 	}
 }
