931ffcb3a8c0f3bc8d52f917e8df76472aeb5dfa
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-04-12 11:47:29 +0000

    -Continued with separated lists. -Added first separated list test (still broken). -Optimized some minor list stuff.

--- SBFGTD/src/gll/IGLL.java
+++ SBFGTD/src/gll/IGLL.java
@@ -4,11 +4,8 @@ import gll.result.INode;
 
 public interface IGLL{
 	public final static int START_SYMBOL_ID = -1;
-	public final static int LIST_CHILD_NOT_FOUND_ID = -2;
 	
-	public final static int LIST_CHILD_FLAG = 0x80000000;
-	public final static int LIST_NEXT_FLAG = 0x40000000;
-	public final static int LIST_LIST_FLAG = 0x20000000;
+	public final static int LIST_LIST_FLAG = 0x80000000;
 	
 	INode parse(String start);
 }
