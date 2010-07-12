7e8a403e067dc54f693161dc248d41d9eaaca339
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-12 09:35:28 +0000

    -More.

--- SBFGTD/src/gll/tests/CILiteral.java
+++ SBFGTD/src/gll/tests/CILiteral.java
@@ -11,14 +11,14 @@ S ::= ci(bla)
 NOTE: ci(*) means whatever * represents is Case Insensitive.
 */
 public class CILiteral extends SGLL{
-	private final static AbstractStackNode LITERAL_a0 = new CaseInsensitiveLiteralStackNode(0, new char[]{'b','l','a'});
+	private final static AbstractStackNode LITERAL_bla0 = new CaseInsensitiveLiteralStackNode(0, new char[]{'b','l','a'});
 	
 	public CILiteral(char[] input){
 		super(input);
 	}
 	
 	public void S(){
-		expect(LITERAL_a0);
+		expect(LITERAL_bla0);
 	}
 	
 	public static void main(String[] args){
