19788368b19a11712bcb5016f63b372e6e39f0e5
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-07-14 08:05:27 +0000

    -Synched.

--- SBFGTD/src/gll/tests/Epsilon.java
+++ SBFGTD/src/gll/tests/Epsilon.java
@@ -2,37 +2,28 @@ package gll.tests;
 
 import gll.SGLL;
 import gll.result.INode;
-import gll.stack.EpsilonStackNode;
-import gll.stack.LiteralStackNode;
-import gll.stack.NonTerminalStackNode;
 import gll.stack.AbstractStackNode;
+import gll.stack.EpsilonStackNode;
 
 /*
-S ::= Aepsilon
-A ::= a
+S ::= epsilon
 */
 public class Epsilon extends SGLL{
-	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
-	private final static AbstractStackNode EPSILON_1 = new EpsilonStackNode(1);
-	private final static AbstractStackNode NONTERMINAL_a2 = new LiteralStackNode(2, new char[]{'a'});
+	private final static AbstractStackNode EPSILON_1 = new EpsilonStackNode(0);
 	
 	public Epsilon(char[] input){
 		super(input);
 	}
 	
 	public void S(){
-		expect(NONTERMINAL_A0, EPSILON_1);
-	}
-	
-	public void A(){
-		expect(NONTERMINAL_a2);
+		expect(EPSILON_1);
 	}
 	
 	public static void main(String[] args){
-		Epsilon e = new Epsilon("a".toCharArray());
+		Epsilon e = new Epsilon(new char[]{});
 		INode result = e.parse("S");
 		System.out.println(result);
 		
-		System.out.println("S(A(a),) <- good");
+		System.out.println("S() <- good");
 	}
 }
