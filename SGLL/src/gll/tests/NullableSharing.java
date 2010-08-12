ed399206d7e8a1f300dcc0d54b42e9011db04188
Arnold Lankamp <arnold.lankamp@gmail.com>
2010-08-12 07:24:33 +0000

    -Added test for shared nullables issue.

---
+++ SGLL/src/gll/tests/NullableSharing.java
@@ -0,0 +1,43 @@
+package gll.tests;
+
+import gll.SGLL;
+import gll.result.AbstractNode;
+import gll.stack.AbstractStackNode;
+import gll.stack.EpsilonStackNode;
+import gll.stack.NonTerminalStackNode;
+
+/*
+* S ::= N N
+* N ::= A
+* A ::= epsilon
+*/
+public class NullableSharing extends SGLL{
+	private final static AbstractStackNode NONTERMINAL_A0 = new NonTerminalStackNode(0, "A");
+	private final static AbstractStackNode NONTERMINAL_N1 = new NonTerminalStackNode(1, "N");
+	private final static AbstractStackNode NONTERMINAL_N2 = new NonTerminalStackNode(2, "N");
+	private final static AbstractStackNode EPSILON3 = new EpsilonStackNode(3);
+	
+	public NullableSharing(char[] input){
+		super(input);
+	}
+	
+	public void S(){
+		expect(NONTERMINAL_N1, NONTERMINAL_N2);
+	}
+	
+	public void A(){
+		expect(EPSILON3);
+	}
+	
+	public void N(){
+		expect(NONTERMINAL_A0);
+	}
+	
+	public static void main(String[] args){
+		NullableSharing ns = new NullableSharing("".toCharArray());
+		AbstractNode result = ns.parse("S");
+		System.out.println(result);
+		
+		System.out.println(" <- good");
+	}
+}
