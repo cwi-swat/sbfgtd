61cbb348ff8d8f5ff9954c63825f85a3e74874d5
Arnold Lankamp <arnold.lankamp@gmail.com>
2011-06-15 11:28:22 +0000

    -Added a piece of code that can identify and pre-process prefix-shared alternatives and is able to construct an expect matrix for them.

---
+++ SBFGTD/src/gtd/preprocessing/ExpectBuilder.java
@@ -0,0 +1,74 @@
+package gtd.preprocessing;
+
+import gtd.stack.AbstractStackNode;
+import gtd.util.ArrayList;
+import gtd.util.LinearIntegerKeyedMap;
+import gtd.util.SortedLinearIntegerKeyedMap;
+
+public class ExpectBuilder{
+	private final SortedLinearIntegerKeyedMap<ArrayList<AbstractStackNode[]>> alternatives;
+	
+	public ExpectBuilder(){
+		super();
+		
+		alternatives = new SortedLinearIntegerKeyedMap<ArrayList<AbstractStackNode[]>>();
+	}
+	
+	public void addAlternative(AbstractStackNode... alternative){
+		int alternativeLength = alternative.length;
+		ArrayList<AbstractStackNode[]> alternativesList = alternatives.findValue(alternativeLength);
+		if(alternativesList == null){
+			alternativesList = new ArrayList<AbstractStackNode[]>();
+			alternatives.add(alternativeLength, alternativesList);
+		}
+		
+		alternativesList.add(alternative);
+	}
+	
+	// Builds the expect matrix and calculates sharing.
+	public AbstractStackNode[][] buildExpectMatrix(){
+		LinearIntegerKeyedMap<AbstractStackNode[]> constructedExpects = new LinearIntegerKeyedMap<AbstractStackNode[]>();
+		
+		for(int i = alternatives.size() - 1; i >= 0; --i){
+			ArrayList<AbstractStackNode[]> alternativesList = alternatives.getValue(i);
+			
+			for(int j = alternativesList.size() - 1; j >= 0; --j){
+				AbstractStackNode[] alternative = alternativesList.get(j);
+				
+				int identifier = alternative[0].getId();
+				AbstractStackNode[] sharedExpect = constructedExpects.findValue(identifier);
+				if(sharedExpect == null){
+					alternative[alternative.length - 1].setProduction(alternative);
+					alternative[alternative.length - 1].markAsEndNode();
+					
+					for(int k = alternative.length - 2; k >= 0; --k){
+						alternative[k].setProduction(alternative);
+					}
+					
+					constructedExpects.add(identifier, alternative);
+				}else{
+					int k = 1;
+					for(; k < alternative.length; ++k){
+						if(alternative[k].getId() != sharedExpect[k].getId()){
+							break;
+						}
+					}
+					
+					if(k < alternative.length){
+						sharedExpect[k - 1].addProduction(alternative);
+					}else{
+						sharedExpect[alternative.length - 1].markAsEndNode();
+					}
+				}
+			}
+		}
+		
+		int nrOfConstructedExpects = constructedExpects.size();
+		AbstractStackNode[][] expectMatrix = new AbstractStackNode[nrOfConstructedExpects][];
+		for(int i = nrOfConstructedExpects - 1; i >= 0; --i){
+			expectMatrix[i] = constructedExpects.getValue(i);
+		}
+		
+		return expectMatrix;
+	}
+}
---
+++ SBFGTD/src/gtd/util/SortedLinearIntegerKeyedMap.java
@@ -0,0 +1,95 @@
+package gtd.util;
+
+public class SortedLinearIntegerKeyedMap<V>{
+	private final static int DEFAULT_SIZE = 8;
+	
+	private int[] keys;
+	private V[] values;
+	
+	private int size;
+	
+	public SortedLinearIntegerKeyedMap(){
+		super();
+		
+		keys = new int[DEFAULT_SIZE];
+		values = (V[]) new Object[DEFAULT_SIZE];
+	}
+	
+	public void enlarge(){
+		int[] oldKeys = keys;
+		keys = new int[size << 1];
+		System.arraycopy(oldKeys, 0, keys, 0, size);
+
+		V[] oldValues = values;
+		values = (V[]) new Object[size << 1];
+		System.arraycopy(oldValues, 0, values, 0, size);
+	}
+	
+	public void add(int key, V value){
+		while(size >= keys.length){
+			enlarge();
+		}
+		
+		for(int i = size - 1; i >= 0; --i){
+			if(keys[i] > key){
+				if(i == size - 1) break;
+				
+				System.arraycopy(keys, i + 1, keys, i + 2, size);
+				System.arraycopy(values, i + 1, values, i + 2, size++);
+				
+				keys[i + 1] = key;
+				values[i + 1] = value;
+				
+				return;
+			}
+		}
+		
+		keys[size] = key;
+		values[size++] = value;
+	}
+	
+	public int getKey(int index){
+		return keys[index];
+	}
+	
+	public V getValue(int index){
+		return values[index];
+	}
+	
+	public int findKey(int key){
+		for(int i = size - 1; i >= 0; --i){
+			if(keys[i] == key){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public V findValue(int key){
+		for(int i = size - 1; i >= 0; --i){
+			if(keys[i] == key){
+				return values[i];
+			}
+		}
+		return null;
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	public int capacity(){
+		return keys.length;
+	}
+	
+	public void clear(){
+		int length = keys.length;
+		keys = new int[length];
+		values = (V[]) new Object[length];
+		size = 0;
+	}
+	
+	public void dirtyClear(){
+		size = 0;
+	}
+}
